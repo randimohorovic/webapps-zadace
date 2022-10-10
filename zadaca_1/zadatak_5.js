6b1ecfdef81c136f603885b7084535dad64cede7
Randi Mohorović <92921082+randimohorovic@users.noreply.github.com>
2022-10-10 21:30:33 +0200

    zadatak 5 rješen

---
+++ zadaca_1/zadatak_5.js
@@ -0,0 +1,11 @@
+let niz = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function djeljenje(m) {
+  for (let x of m) {
+    if (x % 3 == 0) {
+      console.log(x);
+    }
+  }
+}
+
+console.log(djeljenje(niz));
